5741e66dfc32cf103ff686ce9b90c7e867f2c81d
Eloups <121400663+Eloups@users.noreply.github.com>
2024-09-05 15:44:11 +0200

    ✔ Added classes
    Added classes User, List, Game and Category

---
+++ src/js/class.js
@@ -0,0 +1,42 @@
+class User{
+    constructor(id, name, firstname, login, password, lists){
+        this.id = id; //int
+        this.name = name; //string
+        this.firstname = firstname; //string
+        this.login = login; //string
+        this.password = password; //string
+        this.lists = lists; //array
+    }
+
+}
+
+class List{
+    constructor(id, name, favorite, games){
+        this.id = id; //int
+        this.name = name; //string
+        this.favorite = favorite; //boolean
+        this.games = games; //array
+    }
+}
+
+class Game{
+    constructor(id, name, release, editor, price, description, pcRequirement, plateform, image, categories){
+        this.id = id; //int
+        this.name = name; //string
+        this.release = release; //date
+        this.editor = editor; //string
+        this.price = price; //float
+        this.description = description; //string
+        this.pcRequirement = pcRequirement; //array
+        this.plateform = plateform; //array
+        this.image = image; //string
+        this.categories = categories; //array
+    }
+}
+
+class Category{
+    constructor(id, name){
+        this.id = id; //int
+        this.name = name; //string
+    }
+}
