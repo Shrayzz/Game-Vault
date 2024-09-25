02d069ba4aae4cc25530db8b91fffb54cf46da65
Eloups <121400663+Eloups@users.noreply.github.com>
2024-09-25 15:24:50 +0200

    ✔Upgraded DB.js
    - mysql2 Database
    - function to connect, initialise, disconect
    - exemple of a SELECT query
    - Added comments to db.js

--- src/js/db.js
+++ src/js/db.js
@@ -1,59 +1,73 @@
+//----------------------------------DATABASE----------------------------------\\
+
 // Get the client
-const mysql = require('mysql2');
+import mysql from 'mysql2/promise';
 
 /**
- * Function to connect to the database
- * @param {string} host database host
- * @param {string} user database user
- * @param {string} password database password
- * @param {string} database database to use (needed if you dont use init(func) after creating the connection)
+ * Create the connection to database
  * @returns {object} the database connection
  */
-function connect(host, user, password, database) {
-    const connection = mysql.createConnection({
-        host: host,
-        user: user,
-        password: password,
-        database: database,
+// TODO: create pool connection
+async function dbConnect() {
+    const con = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'simplegamelibrary',
     });
-
-    connection.connect(function (err) {
-        if (err) throw err;
-        console.log('succesfully connected to DB');
-    });
-
-    return connection;
+    return con
 }
 
 /**
- * Function to end the connection to the database
- * @param {object} con database connection
+ * Initialise the database by creating the tables if not exists
+ * @param {object} con your connection 
  */
-function disconnect(con) {
+async function dbInit(con) {
+    const loginTable = 'CREATE TABLE IF NOT EXISTS accounts (id int(11) NOT NULL AUTO_INCREMENT, username varchar(50) NOT NULL UNIQUE, password varchar(255) NOT NULL, email varchar(100) NOT NULL UNIQUE, PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;';
+    const listTable = 'CREATE TABLE IF NOT EXISTS list (id int(11) NOT NULL AUTO_INCREMENT, name varchar(50) NOT NULL, favorite boolean DEFAULT false, accountId int(11) NOT NULL, PRIMARY KEY (id), FOREIGN KEY (accountID) REFERENCES accounts (id)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;';
+    const gameTable = 'CREATE TABLE IF NOT EXISTS game(id int(11) NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, `release` date NOT NULL, publishers varchar(50) NOT NULL, developers varchar(50) NOT NULL, price float NOT NULL, rating int, description text, languages text, plateforms json, pcRequirement json, image blob, PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;';
+    const categoryTable = 'CREATE TABLE IF NOT EXISTS category(id int(11) NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, PRIMARY KEY (id)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;'
+    const listHasGameTable = 'CREATE TABLE IF NOT EXISTS listHasGames(idList int(11) NOT NULL, idGame int(11) NOT NULL, PRIMARY KEY (idList, idGame), FOREIGN KEY (idList) REFERENCES list (id), FOREIGN KEY (idGame) REFERENCES game (id)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;';
+    const gameHasCategory = 'CREATE TABLE IF NOT EXISTS gameHasCategory(idGame int(11) NOT NULL, idCategory int(11) NOT NULL, PRIMARY KEY (idGame, idCategory), FOREIGN KEY (idGame) REFERENCES game (id), FOREIGN KEY (idCategory) REFERENCES category (id)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;';
+
+    con.query(loginTable);
+    con.query(listTable);
+    con.query(gameTable);
+    con.query(categoryTable);
+    con.query(listHasGameTable);
+    con.query(gameHasCategory);
+}
+
+/**
+ * Disconnect the Database
+ * @param {object} con your database to disconnect 
+ */
+async function dbDisconnect(con) {
     con.end(function (err) {
         if (err) throw err;
         console.log('connection to DB successfully closed');
     });
 }
 
-/*
-//Exemple de SELECT
-function getAllGames(connection) {
-    connection.query(
-        'SELECT * FROM game',
-        function (err, results) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            console.log(results);
-        }
-    );
+//----------------------------------SELECT----------------------------------\\
+
+/**
+ * Get the games of the database
+ * @param {object} con your database
+ * @returns {array[object]} return all games from the database
+ */
+async function getGames(con) {
+    const [rows] = await con.query('SELECT * FROM game');
+    return rows;
 }
 
-con = connect('localhost', 'root', 'root', 'simplegamelibrary');
+//----------------------------------INSERT----------------------------------\\
 
-getAllGames(con);
 
-disconnect(con);
-*/
+
+//----------------------------------DELETE----------------------------------\\
+
+
+
+//----------------------------------TESTS----------------------------------\\
+
