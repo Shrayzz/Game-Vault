94c125b418f29f68464ce42e3601474b4d46778f
Eloups <121400663+Eloups@users.noreply.github.com>
2024-11-20 14:42:14 +0100

    ➕ added functions to Xbox API
    - added getNewGames
    - added getGameByTitleId

--- src/js/api/xbox.js
+++ src/js/api/xbox.js
@@ -7,7 +7,7 @@ import axios from "axios";
 async function XboxConnect() {
     const instance = axios.create({
         baseURL: 'https://xbl.io/api/v2/',
-        timeout: 10000,
+        timeout: 25000,
         headers: { 'X-Authorization': process.env.XBOX_API_KEY }
     })
     return instance
@@ -15,7 +15,8 @@ async function XboxConnect() {
 
 /**
  * Return your account informations
- * @returns 
+ * @param {object} instance the instance for XboxLive API
+ * @returns {object} profileUsers data
  */
 async function getXboxAccount(instance) {
     try {
@@ -26,4 +27,46 @@ async function getXboxAccount(instance) {
     }
 }
 
-export default { XboxConnect, getXboxAccount }
+/**
+ * return all new games of Xbox marketplace
+ * @param {object} instance the instance for XboxLive API 
+ * @returns {object} new games of Xbox marketplace
+ */
+async function getNewGames(instance) {
+    try {
+        const response = await instance.get('/marketplace/new');
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+/**
+ * return a game with his title ID
+ * @param {object} instance the instance for XboxLive API
+ * @param {string} id the ID of the game
+ * @returns {object} the game of the title ID
+ */
+async function getGameByTitleId(instance, id) {
+    try {
+        const response = await instance.get(`/marketplace/title/${id}`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// async function test() {
+//     const instance = await XboxConnect();
+//     // console.log(await getNewGames(instance));
+//     //Genshin Impact
+//     const game = await getGameByTitleId(instance, '2053080068');
+//     console.log(game.Products[0].LocalizedProperties[0].DeveloperName);
+//     console.log(game.Products[0].LocalizedProperties[0].ProductTitle);
+// };
+// test();
+
+export default {
+    XboxConnect,
+    getXboxAccount,
+}
