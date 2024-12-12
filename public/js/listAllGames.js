c644c4f9912b8ce8082a8f1b225071b0cc0e3a47
Eloups <121400663+Eloups@users.noreply.github.com>
2024-12-12 15:12:57 +0100

    first list of games
    first list of games

--- public/js/listAllGames.js
+++ public/js/listAllGames.js
@@ -1,23 +1,52 @@
-//TODO: Ajouter les jeux avec l'API de steam
-const nameList = document.getElementById('name-list');
-const container = document.getElementById('sContainer');
-
-window.numGame = 0;
-for (window.numGame; window.numGame < 100; window.numGame++) {
-    const newLine = document.createElement("li");
-    newLine.textContent = `Jeux n°${window.numGame}`;
-    nameList.appendChild(newLine);
-}
-const button = document.createElement("button");
-button.textContent = 'Afficher 100 de plus';
-container.appendChild(button);
-
-button.addEventListener('click', function () {
+(async () => {
+    // get the page elements
     const nameList = document.getElementById('name-list');
-    let arret = window.numGame + 100
-    for (window.numGame; window.numGame < arret; window.numGame++) {
-        const newLine = document.createElement("li");
-        newLine.textContent = `Jeux n°${window.numGame}`;
-        nameList.appendChild(newLine);
+    const addButton = document.getElementById('addButton');
+
+    // get all steam games
+    const steamGames = await fetch("http://localhost:3000/api/steam/apps", {
+        method: "GET",
+    });
+    // put the games in the window in JSON
+    window.allGames = await steamGames.json();
+
+    // put an increment in the window
+    window.numGame = 0;
+
+    // list 100 firsts games
+    for (window.numGame; window.numGame < 100; window.numGame++) {
+        // get game list element
+        const nameList = document.getElementById('name-list');
+        // we stop in 100 games
+        let arret = window.numGame + 100
+        // list 100 more games
+        for (window.numGame; window.numGame < arret; window.numGame++) {
+            // add a new line
+            const newLine = document.createElement("li");
+            // get the game ID
+            const gameId = window.allGames[window.numGame]?.appid
+            const gameName = window.allGames[window.numGame]?.name
+            newLine.textContent = `Jeux n°${gameId} : ${gameName}`;
+            nameList.appendChild(newLine);
+        }
     }
-})
+
+    // When button click list 100 more games
+    addButton.addEventListener('click', async function () {
+        // get game list element
+        const nameList = document.getElementById('name-list');
+        // we stop in 100 games
+        let arret = window.numGame + 100
+        // list 100 more games
+        for (window.numGame; window.numGame < arret; window.numGame++) {
+            // add a new line
+            const newLine = document.createElement("li");
+            // get the game ID
+            const gameId = window.allGames[window.numGame]?.appid
+            const gameName = window.allGames[window.numGame]?.name
+            newLine.textContent = `Jeux n°${gameId} : ${gameName}`;
+            nameList.appendChild(newLine);
+        }
+    })
+
+})()
