539cb6a86e7f16462a071280898def00965f46f7
Zigleur <106914220+Zigl3ur@users.noreply.github.com>
2024-12-12 14:03:40 +0100

    Revert "✔️ Added first responsive base on index (without menu context), updated enter events in account pages"
    This reverts commit 2fd21b4c7c4a70751f42a9178721cefb18c89364.

--- public/js/listAllGames.js
+++ public/js/listAllGames.js
@@ -2,23 +2,23 @@
 const nameList = document.getElementById('name-list');
 const container = document.getElementById('sContainer');
 
-let i = 0; //! On cemmence à l'index 0
+let i = 0;
 for (i; i < 100; i++) {
     const newLine = document.createElement("li");
     newLine.textContent = `Jeux n°${i}`;
     nameList.appendChild(newLine);
 }
 const button = document.createElement("button");
-button.textContent = 'More Games (+100)';
+button.textContent = 'Afficher 100 de plus';
 container.appendChild(button);
 
-button.addEventListener('click', () => {
+button.addEventListener('click', function () {
     showMoreGames();
 })
 
 function showMoreGames() {
     const nameList = document.getElementById('name-list');
-    i = 0; //! On cemmence à l'index 0
+    i = 0
     for (i; i < 100; i++) {
         const newLine = document.createElement("li");
         newLine.textContent = `Jeux n°${i}`;
