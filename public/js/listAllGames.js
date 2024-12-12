decdcfbe6f175150658c14fbe02b84ff9c24e29e
Eloups <121400663+Eloups@users.noreply.github.com>
2024-12-12 14:39:43 +0100

    Update listAllGames.js
    Update listAllGames.js

--- public/js/listAllGames.js
+++ public/js/listAllGames.js
@@ -2,10 +2,10 @@
 const nameList = document.getElementById('name-list');
 const container = document.getElementById('sContainer');
 
-let i = 0;
-for (i; i < 100; i++) {
+window.numGame = 0;
+for (window.numGame; window.numGame < 100; window.numGame++) {
     const newLine = document.createElement("li");
-    newLine.textContent = `Jeux n°${i}`;
+    newLine.textContent = `Jeux n°${window.numGame}`;
     nameList.appendChild(newLine);
 }
 const button = document.createElement("button");
@@ -13,15 +13,11 @@ button.textContent = 'Afficher 100 de plus';
 container.appendChild(button);
 
 button.addEventListener('click', function () {
-    showMoreGames();
-})
-
-function showMoreGames() {
     const nameList = document.getElementById('name-list');
-    i = 0
-    for (i; i < 100; i++) {
+    let arret = window.numGame + 100
+    for (window.numGame; window.numGame < arret; window.numGame++) {
         const newLine = document.createElement("li");
-        newLine.textContent = `Jeux n°${i}`;
+        newLine.textContent = `Jeux n°${window.numGame}`;
         nameList.appendChild(newLine);
     }
-}
+})
