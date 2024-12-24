6cf7200eb547a8ac1e3ffc5663a5df44e333040d
Eloups <121400663+Eloups@users.noreply.github.com>
2024-12-24 19:15:56 +0100

    deleted comments

--- src/js/utils/profile.js
+++ src/js/utils/profile.js
@@ -33,16 +33,10 @@ async function deleteAccount(req, pool) {
 
     if (existUser) {
         const lists = await db.getUserLists(pool, username);
-        // await lists.forEach(async (list) => {
-        //     await db.deleteList(pool, list?.id);
-        // })
-        // console.log(lists)
-
         while (lists.length !== 0) {
             await db.deleteList(pool, lists[0]?.id);
             lists.shift();
         }
-        // while(lists.lenght)
         await db.deleteUser(pool, username);
     } else {
         return new Response("User does not exist", { status: 502 });
