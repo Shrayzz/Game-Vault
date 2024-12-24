938f58897abf6a9b1d7b8426978a03944b55c9dd
Eloups <121400663+Eloups@users.noreply.github.com>
2024-12-24 13:46:01 +0100

    fix list add
    fix list add while getting the username

--- public/js/listGame.js
+++ public/js/listGame.js
@@ -21,7 +21,19 @@ document.addEventListener("DOMContentLoaded", () => {
         newListItem.style.fontWeight = "bold";
         newListItem.style.color = "#000";
 
-        const username = localStorage.getItem("username");
+        const cookieString = document.cookie;
+        const cookieParts = cookieString.split('; ');
+        let cookies = [];
+        for (const part of cookieParts) {
+          cookies.push(part.split('='));
+        }
+
+        cookies.forEach((cookie) => {
+          if (cookie[0] === "username") {
+            window.username = cookie[1]
+          }
+        })
+        const username = window.username
 
         const response = await fetch(
           "http://localhost:3000/api/addList",
